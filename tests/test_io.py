429914c17c8e68d904b54ce2a11c8902876e8732
Michael Helmling <michaelhelmling@posteo.de>
2019-03-26 21:19:56 +0100

    install pytest and output getuid() result

--- tests/test_io.py
+++ tests/test_io.py
@@ -44,6 +44,7 @@ def test_os_error_on_save_read_only_file(tmpdir):
 def test_file_with_non_ascii_name_throws_on_readonly_save(tmpdir):
     """Motivated by https://github.com/supermihi/pytaglib/issues/21.
     """
+    print(os.getuid())
     copy_file = copy_test_file('readönly.mp3', tmpdir)
     os.chmod(copy_file, stat.S_IREAD)
     tfile = taglib.File(copy_file.encode('utf8'))
